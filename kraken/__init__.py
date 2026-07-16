d54da84525b70bc6cbca68689d9e6d82a710436b
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2026-07-16 10:12:16 -0500

    Add error handling for server execution to gracefully handle KeyboardInterrupt

--- kraken/__init__.py
+++ kraken/__init__.py
@@ -1,4 +1,7 @@
 from kraken.server import kraken
 
 def main():
-    kraken.run(transport="http", host="0.0.0.0", port=8000)
+    try:
+        kraken.run(transport="http", host="0.0.0.0", port=8000)
+    except KeyboardInterrupt:
+        print("Server stopped by user.")
