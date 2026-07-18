91b890f8bc45d79f8f57c3327c34f8c163a91671
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2026-07-18 09:30:33 -0500

    Update port number for server execution from 8000 to 8001

--- kraken/__init__.py
+++ kraken/__init__.py
@@ -2,6 +2,6 @@ from kraken.server import kraken
 
 def main():
     try:
-        kraken.run(transport="http", host="0.0.0.0", port=8000)
+        kraken.run(transport="http", host="0.0.0.0", port=8001)
     except KeyboardInterrupt:
         print("Server stopped by user.")
