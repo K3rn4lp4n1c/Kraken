975b9317ab556900054b52d5b0259a7fc2b4f618
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2026-07-19 20:15:57 -0500

    Add send_request import to powderboy module for enhanced functionality

--- flyingdutchman/powderboy/__init__.py
+++ flyingdutchman/powderboy/__init__.py
@@ -1,5 +1,5 @@
 LOGGER_HANDLER_MARKER = "flyingdutchman_logging_handler"
-from .tools import configure_logger, env, sqlite3_connect
+from .tools import configure_logger, env, sqlite3_connect, send_request
 from .extensions import PlaywrightManager
 
 __all__ = ["env", "sqlite3_connect"]
