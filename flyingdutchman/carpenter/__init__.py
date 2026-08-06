f00f2edb67ae0c2567403e031991d80b2d11b5bc
HeIsOdin <158656894+Odin3141@users.noreply.github.com>
2026-08-06 01:15:21 -0500

    Initialize FastMCP in Carpenter module and update import from tools

--- flyingdutchman/carpenter/__init__.py
+++ flyingdutchman/carpenter/__init__.py
@@ -1,4 +1,8 @@
 LOGGER_HANDLER_MARKER = "flyingdutchman_logging_handler"
-from .tools import *
+from fastmcp import FastMCP
+
+carpenter = FastMCP(name="Carpenter")
+
+from .utils import *
 from .extensions import *
 from .plugins import *
