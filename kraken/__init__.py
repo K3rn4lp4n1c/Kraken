8d921653d3bd01c23946664635ef4bc4f3f993be
Benjamin <benjaminadedowole@gmail.com>
2026-08-06 04:18:53 -0500

    Add MCP_SERVER constant
    Added MCP_SERVER constant to __init__.py

--- kraken/__init__.py
+++ kraken/__init__.py
@@ -1,7 +1,1 @@
-from kraken.server import kraken
-
-def main():
-    try:
-        kraken.run(transport="http", host="0.0.0.0", port=8001)
-    except KeyboardInterrupt:
-        print("Server stopped by user.")
+MCP_SERVER = "flyingdutchman"
